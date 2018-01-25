d58fd012b77196163644b6db40675e39824bf58a
Farida Farsian <ffarsian@sissa.it>
2018-01-25 19:48:44 +0100

    added kinetic test

--- src/unittest.py
+++ src/unittest.py
@@ -14,7 +14,7 @@ class Test(unittest.TestCase):
 
     def test_input(self):
         s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+        self.assertEqual()
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
