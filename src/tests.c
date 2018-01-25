c56854bfd6f0b4a549de1234448cb991a4f49876
Farida Farsian <ffarsian@sissa.it>
2018-01-25 16:38:35 +0100

    first version of unittest

--- src/tests.c
+++ src/tests.c
@@ -1,1 +1,38 @@
 /*tests.c: source file where the test functions are implemented*/
+#include <ljmd.h>
+#include <stdio.h>
+
+struct _mdsys {
+    int natoms,nfi,nsteps;
+    double dt, mass, epsilon, sigma, box, rcut;
+    double ekin, epot, temp;
+    double *rx, *ry, *rz;
+    double *vx, *vy, *vz;
+    double *fx, *fy, *fz;
+};
+typedef struct _mdsys mdsys_t;
+//input test
+void test2(){
+
+}
+
+
+int main(int argc, char **argv)
+{
+  double k;
+  force();
+  printf();
+
+  verlet_1();
+  verlet_2();
+  printf("time of full step integration is done!");
+  
+  k = ekin();
+  printf("kinetic energy is %f", k);
+  
+  if()
+    printf("input parameter data is read correctly");
+    return 0;
+    
+    return 1;
+}
---
+++ src/unittest.py
@@ -0,0 +1,23 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_force(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def Integration(self):
+        self.assert()
+            
+    def test_kinetic(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_input(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
