e60678e68bd30e981da06690ae122774c31d3fbf
Farida Farsian <ffarsian@sissa.it>
2018-01-28 23:05:59 +0100

    first edition of python wrapper

--- PythonTests/Ljmd.py
+++ PythonTests/Ljmd.py
@@ -3,6 +3,12 @@
 import unittest
 from ctypes import *
 
+dso  = CDLL("./ljmd.so")
+print ("Calling DSO \n")
+
+nprint = c_int()
+BLEN = 200
+
 class mdsys(Structure):
     _fields_ =[("natoms", c_int),("nfi", c_int),("nsteps", c_int),\
                ("dt",c_double),("mass",c_double),("epsilon",c_double),("sigma",c_double),("box",c_double),("rcut",c_double),\
@@ -11,46 +17,43 @@ class mdsys(Structure):
                ("vx",POINTER(c_double)),("vy",POINTER(c_double)),("vz",POINTER(c_double)),\
                ("fx",POINTER(c_double)),("fy",POINTER(c_double)),("fz",POINTER(c_double))]
 
-# I should have input as number of atom for allocation of types
-# definition of struct and another for testing
-# the interface should be just for heavier functions as force, ververlet.
-    
+sys = mdsys( )
 
-# import shared object
-dso = CDLL("./ljmd.so")
+restfile = create_string_buffer(BLEN)
+trajfile = create_string_buffer(BLEN)
+ergfile = create_string_buffer(BLEN)
+line = create_string_buffer(BLEN)
 
-print ("Calling DSO")
+dso.set_mdsys(byref(sys),restfile,trajfile,ergfile,line,byref(nprint))
 
-with open('argon_108.inp', 'r') as input:
-    for line in input:
-        sys = line.spilit('#')
-#    input.readline() 
+dso.allocate_mdsys(byref(sys))
 
+#dso.set_ic(byref(sys),restfile)
 
+#sys.nfi = c_int(0)
+#dso.force(byref(sys))
+#dso.ekin(byref(sys))
 
+#open(ergfile,'w') as erg
+#open(trajfile, 'w') as traj
 
+#dso.output(byref(sys),erg,traj)
 
-class Test(unittest.TestCase):
+################################
 
-    def test_force(self):
-        dso.force()
-        self.assertEqual('foo'.upper(), 'FOO')
+#if ((sys.nfi % nprint) == 0):
+#    dso.output(&sys, erg, traj)
 
-    def test_Integration(self):
-        dso.verlet_1()
-        dso.verlet_2()
-        self.assert()
-            
-    def test_kinetic(self):
-        dso.ekin()
-        self.assertTrue()
-        self.assertFalse()
+#dso.velverlet_1(byref(sys))
+#dso.force(byref(sys))
+#dso.velverlet_2(byref(sys))
+#dso.ekin(byref(sys))
 
-    def test_input(self):
-        self.assertEqual()
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+################################
 
-if __name__ == '__main__':
-    unittest.main()
+#print("Simulation Done.\n")
+
+#close(erg)
+#close(traj)
+
+#dso.free_mdsys(byref(sys))
