8162908523ebd0823c8de1c24bb84e0475aba21e
Timoteo Colnaghi <project-tuva@users.noreply.github.com>
2018-01-30 18:32:01 +0100

    working. ests to be finished

--- tests/force_test.c
+++ tests/force_test.c
@@ -14,13 +14,28 @@ force2.test: if r in [0.05, 0.1) B<->C
 force3.test: if r in [0.1, 0.395) B<->C & A<->D
 */
 
-
+#ifdef  _MPI
+#include <mpi.h>
+#endif
 
 #include <ljmd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void){
+
+  int rank=0;
+  int size = 1;
+
+#ifdef _MPI
+  // INITIALIZE MPI ENVIRONMENT                                                                                    
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+#endif /*_MPI*/
+
+
+
   int nprint;
   char restfile[BLEN], trajfile[BLEN], ergfile[BLEN], line[BLEN];
   mdsys_t sys;
@@ -35,6 +50,15 @@ int main(void){
   test_output(&sys, trajfile);
 
   free_mdsys(&sys);
+
+
+
+#ifdef _MPI
+  //MPI FINALIZE                                                                                                 
+  MPI_Finalize();
+#endif /*_MPI*/
+
+
 
   return 0;
 }
