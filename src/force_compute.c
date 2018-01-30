590af4ba079d98f68297b87e14a8f6232e7da3a0
ciuffreda <ciuffredaluca@gmail.com>
2018-01-30 16:28:15 +0100

    omp not working

--- src/force_compute.c
+++ src/force_compute.c
@@ -19,53 +19,46 @@ void force(mdsys_t *sys){
     sys->nthreads=1;
     double epot=0;
     
-#if defined(_OPENMP)
 #pragma omp parallel reduction(+:epot)
     {
-    tid=omp_get_thread_num();
-    sys->nthreads=omp_get_num_threads();
-    epot=tid;
+#if defined(_OPENMP)
+      tid=omp_get_thread_num();
+      sys->nthreads=omp_get_num_threads();
+#endif      
 #pragma omp for private(i,j)
-#endif
- 
-    for(i=0; i < (sys->natoms); i+=sys->nthreads) {
-      int ii=i+tid;
-      if(ii>=sys->natoms);
-      else{
-      for(j=0; j < (sys->natoms); ++j) {
+      for(i=0; i < (sys->natoms); i+=sys->nthreads) {
+	int ii=i+tid;
+	if(ii<sys->natoms){
+	  for(j=0; j < (sys->natoms); ++j) {
 
-	/* particles have no interactions with themselves */
-	if (ii==j) continue;
+	    /* particles have no interactions with themselves */
+	    if (ii==j) continue;
 
-	/* get distance between particle i and j */
-	rx=pbc(sys->rx[ii] - sys->rx[j], 0.5*sys->box);
-	ry=pbc(sys->ry[ii] - sys->ry[j], 0.5*sys->box);
-	rz=pbc(sys->rz[ii] - sys->rz[j], 0.5*sys->box);
-	r = sqrt(rx*rx + ry*ry + rz*rz);
+	    /* get distance between particle i and j */
+	    rx=pbc(sys->rx[ii] - sys->rx[j], 0.5*sys->box);
+	    ry=pbc(sys->ry[ii] - sys->ry[j], 0.5*sys->box);
+	    rz=pbc(sys->rz[ii] - sys->rz[j], 0.5*sys->box);
+	    r = sqrt(rx*rx + ry*ry + rz*rz);
 
-	/* compute force and energy if within cutoff */
-	if (r < sys->rcut) {
-	  ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
-				    +6*pow(sys->sigma/r,6.0)/r);
+	    /* compute force and energy if within cutoff */
+	    if (r < sys->rcut) {
+	      ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
+					+6*pow(sys->sigma/r,6.0)/r);
+#pragma omp critical
+{  
+	      epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
+					    -pow(sys->sigma/r,6.0));
+} 
 
-	  epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
-					-pow(sys->sigma/r,6.0));
-
-	  sys->fx[ii] += rx/r*ffac;
-	  sys->fy[ii] += ry/r*ffac;
-	  sys->fz[ii] += rz/r*ffac;
-	}
+	      sys->fx[ii] += rx/r*ffac;
+	      sys->fy[ii] += ry/r*ffac;
+	      sys->fz[ii] += rz/r*ffac;
+	    }
+	  }
+	}else;
       }
     }
-       }
-
-
-#if defined(_OPENMP)
-       }
-    }
-#endif     
 
   sys->epot=epot;
-
     
 }
