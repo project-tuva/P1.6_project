c37f458619916af6996d5dff02da15e6a57e5b93
ciuffreda <ciuffredaluca@gmail.com>
2018-01-30 19:58:41 +0100

    mixed force_compute

--- src/force_compute.c
+++ src/force_compute.c
@@ -2,6 +2,7 @@
 /*force_compute.c*/
 #include <ljmd.h>
 #include <math.h>
+#include<omp.h>
 
 static double pbc(double x, const double boxby2){
   while (x >  boxby2) x -= 2.0*boxby2;
@@ -13,7 +14,7 @@ static double pbc(double x, const double boxby2){
 void force(mdsys_t *sys){
 
   double ffac;
-  double rx,ry,rz;
+  double rx,ry,rz,rsq,rinv,r6;
   int i,j;
 
   /* zero energy and forces */
@@ -25,8 +26,12 @@ void force(mdsys_t *sys){
   double epot=0;
   int tid=0;
 
+  double c12 = 4.0*sys->epsilon*pow(sys->sigma, 12.0);
+  double c6 = 4.0*sys->epsilon*pow(sys->sigma, 6.0);
+  double rcsq = sys->rcut* sys->rcut;
+
 #if defined(_OPENMP)
-#pragma omp parallel reduction(+:epot) private(i,j,ffac,r,rx,ry,rz)
+#pragma omp parallel reduction(+:epot) private(i,j,ffac,rsq,rx,ry,rz,rinv,r6)
   {
     tid=omp_get_thread_num();
 #endif
@@ -38,11 +43,11 @@ void force(mdsys_t *sys){
 	  rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
 	  ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
 	  rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
-	  double rsq = rx*rx + ry*ry + rz*rz;
+	  rsq = rx*rx + ry*ry + rz*rz;
 
 	  if (rsq < rcsq) {
-	    double rinv = 1.0/rsq;
-	    double r6 = rinv*rinv*rinv;
+	    rinv = 1.0/rsq;
+	    r6 = rinv*rinv*rinv;
 	    
 	    ffac = (12.0*c12*r6-6.0*c6)*r6*rinv;
 
